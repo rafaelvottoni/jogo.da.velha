2951850624c5fd5ccbe328eb7748d0d6a7f060ae
rafaelvottoni <rafaelottoni@gmail.com>
2021-07-02 13:06:12 -0300

    add load game modal

---
+++ scripts/html.js
@@ -0,0 +1,14 @@
+const htmlHomePage = `
+  <div class="container">
+    <header>
+      <h1 class="logo">
+        Jogo da Velh<i class="icon-hash"></i>
+      </h1>
+    </header>
+
+    <div class="buttons">
+      <button class="button">Novo Jogo</button>
+      <button class="button">Carregar</button>
+    </div>
+  </div>
+`
